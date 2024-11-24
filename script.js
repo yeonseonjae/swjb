b5f3b23a3bf19919704083c8717796b4d1352caf
yeonseonjae <ysj080822@gmail.com>
2024-11-25 02:00:31 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-$(".project .images .snk-1").click(function() {
-    $(".img-back").fadeIn();
-    $(".img-back .images .snk-1").fadeIn();
+$(document).ready(function () {
+    $('.image-modal').hide();
 })
+
+$('.project .box img').click(function () {
+    const imgSrc = $(this).attr('src');
+    $('.image-modal img').attr('src', imgSrc);
+    $('.image-modal').fadeIn();
+});
+
+$('.image-modal').click(function () {
+    $(this).fadeOut();
+});
+
+$(document).keydown(function (e) {
+    if (e.key === "Escape") {
+        $('.image-modal').fadeOut();
+    }
+});
